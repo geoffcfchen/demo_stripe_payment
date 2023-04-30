c9e6c5b7ce3a822e4fdf8d126b481c50e9ef0498
chinfanster <chinfanster@gmail.com>
2023-04-30 00:39:05 -0700

    finish fetchPyamentintent function

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StripeApp from "./src/StripeApp";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider publishableKey="pk_test_51Lhf4GDsOD7fAAq8BAQLfXxnP69pNkOgwcX8CbYx5YEsqzQWHEVFbKoAIjetsXCyQzq46U73S4fEQBOeJLo6inea00vzpGOQet">
+      <StripeApp></StripeApp>
+    </StripeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/StripeApp.js
@@ -0,0 +1,75 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { CardField, useConfirmPayment } from "@stripe/stripe-react-native";
+
+export default function StripeApp() {
+  const [email, setEmail] = useState("");
+  const [cardDetails, setCardDetails] = useState("");
+  const { confirPayment, loading } = useConfirmPayment();
+
+  async function fetchPaymentIntentClientSecret() {
+    const response = await fetch(`${API_URL}/create-payment-intent`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { clientSecret, error } = await response.json();
+    return { clientSecret, error };
+  }
+
+  async function handlePayPress() {
+    // 1. Gather the customer's billing information (e.g., email)
+    if (!cardDetails?.complete || !email) {
+      Alert.alert("Please ener Complete card details and Email");
+      return;
+    }
+    const billingDetails = {
+      email: email,
+    };
+  }
+  return (
+    <View style={styles.container}>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="E-mail"
+        keyboardType="email-address"
+        onChange={(value) => setEmail(value.nativeEvent.text)}
+        style={styles.input}
+      ></TextInput>
+      <CardField
+        postalCodeEnabled={true}
+        placeholders={{ number: "4242 4242 4242 4242" }}
+        cardStyle={styles.card}
+        style={styles.cardContainer}
+        onCardChange={(cardDetails) => {
+          setCardDetails(cardDetails);
+        }}
+      ></CardField>
+      <Button onPress={handlePayPress} title="Pay" disabled={loading}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    margin: 20,
+    justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "#efefefef",
+    borderRadius: 8,
+    fontSize: 20,
+    height: 50,
+    padding: 10,
+  },
+  card: {
+    backgroundColor: "#efefefef",
+  },
+  cardContainer: {
+    height: 50,
+    marginVertical: 30,
+  },
+});
